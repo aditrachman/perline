4b8e245d076dff2f854872317a103cfa790eb22c
Aditrachman23 <57661509+aditrachman@users.noreply.github.com>
2020-02-21 16:54:52 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="id">
+  
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>PERLINE</title>
+    <link rel="icon" href="img/core-img/favicon.ico">
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <!-- Header Area-->
+  <?php
+  include("template/navbar.php")
+  ?>
+    <?php
+  include("template/kepala.php")
+  ?>
+
+    <!-- Extra Features Area-->
+    <section class="main_features_area bg-gray section_padding_130">
+      <div class="container">
+        <div class="row align-items-center justify-content-between">
+          <div class="col-12 col-sm-10 col-md-6">
+            <!-- Section Heading-->
+            <div class="section_heading wow fadeInUp" data-wow-delay="0.2s">
+              <h6>PERLINE</h6>
+              <h3>kalo nyari disini kamu gausa <span>bingung</span> deh</h3>
+              <div class="line mr-auto ml-0"></div>
+            </div>
+            <!-- Single Item-->
+            <div class="single_benifits d-flex wow fadeInUp" data-wow-delay="0.2s">
+              <div class="icon_box mr-30"><i class="lni-bi-cycle"></i></div>
+              <div class="single_benifits_text">
+                <h5>Enteng coy</h5>
+                <p>Ni website kaga berat jadi ni web <br> enteng di device lu </p>
+              </div>
+            </div>
+            <!-- Single Item-->
+            <div class="single_benifits d-flex wow fadeInUp" data-wow-delay="0.2s">
+              <div class="icon_box mr-30"><i class="lni-color-pallet"></i></div>
+              <div class="single_benifits_text">
+                <h5>Desain nye</h5>
+                <p>Cakep banget kan, adem ayem gitu <br> kek tau kalo doi juga suka ma elu</p>
+              </div>
+            </div>
+            <!-- Single Item-->
+            <div class="single_benifits d-flex wow fadeInUp" data-wow-delay="0.2s">
+              <div class="icon_box mr-30"><i class="lni-flower"></i></div>
+              <div class="single_benifits_text">
+                <h5>Responsif</h5>
+                <p>bisa di hp juga lho keren ga tuh, tapi <br> masi bagus di lepi</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-12 col-md-6 col-lg-5">
+            <div class="main_features_thumbnail mt-5 mt-md-0"><img src="img/bg-img/hero-4.png" alt=""></div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Features Area-->
+    <section class="using_benefits_area section_padding_130" id="benefits">
+      <!-- Benefit Top Background-->
+      <div class="benefit-top-thumbnail"><img src="img/core-img/video-bottom.png" alt=""></div>
+      <!-- Benefit Top Background-->
+      <div class="benefit-bottom-thumbnail"><img src="img/core-img/benefit-bottom.png" alt=""></div>
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-12 col-sm-8 col-lg-6">
+            <!-- Section Heading-->
+            <div class="section_heading white text-center wow fadeInUp" data-wow-delay="0.2s">
+              <h6>PERLINE</h6>
+              <h3>Nanti kita cerita tentang hari ini | terus kapan cerita nya kampret</h3>
+              <div class="line"></div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12 text-center wow fadeInUp" data-wow-delay="0.2s"><a class="btn apland-btn" href="tentangkita.php">Tentang kita</a></div>
+        </div>
+      </div>
+    </section>
+    
+
+    
+<?php
+  include("template/footer.php")
+  ?>
+
+   
+
+    <!-- jQuery(necessary for all JavaScript plugins)-->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/waypoints.min.js"></script>
+    <script src="js/jquery.easing.min.js"></script>
+    <script src="js/default/classy-nav.min.js"></script>
+    <script src="js/default/sticky.js"></script>
+    <script src="js/default/mail.js"></script>
+    <script src="js/default/scrollup.min.js"></script>
+    <script src="js/default/one-page-nav.js"></script>
+    <script src="js/jarallax.min.js"></script>
+    <script src="js/jarallax-video.min.js"></script>
+    <script src="js/jquery.counterup.min.js"></script>
+    <script src="js/jquery.countdown.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/wow.min.js"></script>
+    <script src="js/default/active.js"></script>
+  </body>
+
+<!-- Mirrored from demo.designing-world.com/apland-4.3.0/apland-khulna.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 20 Feb 2020 08:48:42 GMT -->
+</html>
---
+++ tentangkita.php
@@ -0,0 +1,285 @@
+<!DOCTYPE html>
+<html lang="en">
+  
+<!-- Mirrored from demo.designing-world.com/apland-4.3.0/about-us.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 20 Feb 2020 08:48:53 GMT -->
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags-->
+    <!-- Title-->
+    <title>Apland - App Landing Page Template</title>
+    <!-- Favicon-->
+    <link rel="icon" href="img/core-img/favicon.ico">
+    <!-- Stylesheet-->
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <!-- Header Area-->
+    <?php
+    include("template/navbar.php")
+    ?>
+
+<?php
+  include("template/kepala1.php")
+  ?>
+    <!-- About Us Area-->
+    <div class="about-us-area section_padding_130">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 col-md-6 col-lg-7">
+            <div class="about-us-content mb-100">
+              <h6>kamu mau tau tentang kita...?</h6>
+              <h2>Kalo kamu mau tau tentang kita, bole di klik ko</h2><a class="btn apland-btn" href="#">klik sini euy biar tau</a>
+            </div>
+          </div>
+          <div class="col-12 col-md-6 col-lg-5">
+            <div class="about-us-content mb-100">
+              <p>kalo gamau tau juga gapapa ko kan ga maksa buat tau kita hehe </p>
+            </div>
+          </div>
+        </div>
+
+    <!-- Our Advisor Area-->
+    <section class="our_advisor_area section_padding_130_80" id="advisor">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-12 col-sm-8 col-lg-6">
+            <!-- Section Heading-->
+            <div class="section_heading text-center wow fadeInUp" data-wow-delay="0.2s">
+              <h3>KAMI <span> POWER RANGERS</span></h3>
+              <p>yang satu kerad yang satu kerad nya parah yang satu kemana yang satu dimana ?</p>
+              <div class="line"></div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <!-- Single Advisor-->
+          <div class="col-12 col-sm-6 col-lg-3">
+            <div class="single_advisor_profile wow fadeInUp" data-wow-delay="0.2s">
+              <!-- Team Thumb-->
+              <div class="advisor_thumb"><img src="img/advisor-img/1.png" alt="">
+                <!-- Social Info-->
+                <div class="social-info"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-linkedin"></i></a></div>
+              </div>
+              <!-- Team Details-->
+              <div class="single_advisor_details_info">
+                <h6>SI MERAH</h6>
+                <p class="designation">Founder &amp; CEO</p>
+              </div>
+            </div>
+          </div>
+          <!-- Single Advisor-->
+          <div class="col-12 col-sm-6 col-lg-3">
+            <div class="single_advisor_profile wow fadeInUp" data-wow-delay="0.3s">
+              <!-- Team Thumb-->
+              <div class="advisor_thumb"><img src="img/advisor-img/1.png" alt="">
+                <!-- Social Info-->
+                <div class="social-info"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-linkedin"></i></a></div>
+              </div>
+              <!-- Team Details-->
+              <div class="single_advisor_details_info">
+                <h6>SI HITAM</h6>
+                <p class="designation">UI Designer</p>
+              </div>
+            </div>
+          </div>
+          <!-- Single Advisor-->
+          <div class="col-12 col-sm-6 col-lg-3">
+            <div class="single_advisor_profile wow fadeInUp" data-wow-delay="0.4s">
+              <!-- Team Thumb-->
+              <div class="advisor_thumb"><img src="img/advisor-img/1.png" alt="">
+                <!-- Social Info-->
+                <div class="social-info"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-linkedin"></i></a></div>
+              </div>
+              <!-- Team Details-->
+              <div class="single_advisor_details_info">
+                <h6>SI BIRU</h6>
+                <p class="designation">Developer</p>
+              </div>
+            </div>
+          </div>
+          <!-- Single Advisor-->
+          <div class="col-12 col-sm-6 col-lg-3">
+            <div class="single_advisor_profile wow fadeInUp" data-wow-delay="0.5s">
+              <!-- Team Thumb-->
+              <div class="advisor_thumb"><img src="img/advisor-img/1.png" alt="">
+                <!-- Social Info-->
+                <div class="social-info"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-linkedin"></i></a></div>
+              </div>
+              <!-- Team Details-->
+              <div class="single_advisor_details_info">
+                <h6>SI KUNING</h6>
+                <p class="designation">Marketing Manager</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="section_heading text-center wow fadeInUp" data-wow-delay="0.2s">
+              <h3>KAMI MEMBUTUHKAN PARA <br> <span> POWER RANGERS </span> SELANJUTNYA</h3>
+              <div class="copywrite-text mb-5">
+              <p>Kalo ada yang yang mau jadi rangers lainnya boleh banget dah <br> kan lumayan banget hehehe, bisa dihubungi<a class="ml-1" href="aditrachman.github.io">disini</a><p>
+              </div>
+              <div class="line"></div>
+            </div>
+    </section>
+
+      </div>
+    </div>
+
+
+    
+    <!-- Service Area-->
+    <div class="service-area section_padding_130_80">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 col-md-6 col-lg-5">
+            <div class="section_heading">
+              <h6>The Bright Feature</h6>
+              <h3>A simple, smart &amp; <span>proven way</span> to boost your site performance.</h3>
+              <div class="line ml-0"></div>
+            </div>
+          </div>
+          <div class="col-12 col-lg-7">
+            <div class="row">
+              <!-- Single Service Area-->
+              <div class="col-12 col-sm-6">
+                <div class="single-service-area mb-50"><i class="lni-bolt"></i>
+                  <h4>Infinite Features</h4>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates officiis nihil doloremque praesentium.</p>
+                </div>
+              </div>
+              <!-- Single Service Area-->
+              <div class="col-12 col-sm-6">
+                <div class="single-service-area mb-50"><i class="lni-bi-cycle"></i>
+                  <h4>Simple &amp; Lightweight</h4>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates officiis nihil doloremque praesentium.</p>
+                </div>
+              </div>
+              <!-- Single Service Area-->
+              <div class="col-12 col-sm-6">
+                <div class="single-service-area mb-50"><i class="lni-leaf"></i>
+                  <h4>Flexible</h4>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates officiis nihil doloremque praesentium.</p>
+                </div>
+              </div>
+              <!-- Single Service Area-->
+              <div class="col-12 col-sm-6">
+                <div class="single-service-area mb-50"><i class="lni-flower"></i>
+                  <h4>Stunning Design</h4>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates officiis nihil doloremque praesentium.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- CTA Area-->
+    <div class="cta-area section_padding_130_80">
+      <div class="container">
+        <div class="row align-items-center justify-content-between">
+          <div class="col-12 col-sm-8">
+            <div class="cta-content mb-50">
+              <h3 class="mb-0">Have questions, or want to talk about working with us?</h3>
+            </div>
+          </div>
+          <div class="col-12 col-sm-4 text-sm-right"><a class="btn apland-btn btn-4 mb-50" href="#">Let's talk Us</a></div>
+        </div>
+      </div>
+    </div>
+    <!-- Footer Area-->
+    <footer class="footer_area section_padding_130_0">
+      <div class="container">
+        <div class="row">
+          <!-- Single Widget-->
+          <div class="col-12 col-sm-6 col-lg-4">
+            <div class="single-footer-widget section_padding_0_130">
+              <!-- Footer Logo-->
+              <div class="footer-logo mb-3"><a href="index-2.html"><img src="img/core-img/logo.png" alt=""></a></div>
+              <p>Appland is completely creative, lightweight, clean app landing page.</p>
+              <!-- Copywrite Text-->
+              <div class="copywrite-text mb-5">
+                <p class="mb-0">Made with <i class="lni-heart mr-1"></i>by<a class="ml-1" href="https://wrapbootstrap.com/user/DesigningWorld">Designing World</a></p>
+              </div>
+              <!-- Footer Social Area-->
+              <div class="footer_social_area"><a href="#" data-toggle="tooltip" data-placement="top" title="Facebook"><i class="fa fa-facebook"></i></a><a href="#" data-toggle="tooltip" data-placement="top" title="Pinterest"><i class="fa fa-pinterest"></i></a><a href="#" data-toggle="tooltip" data-placement="top" title="Skype"><i class="fa fa-skype"></i></a><a href="#" data-toggle="tooltip" data-placement="top" title="Twitter"><i class="fa fa-twitter"></i></a></div>
+            </div>
+          </div>
+          <!-- Single Widget-->
+          <div class="col-12 col-sm-6 col-lg">
+            <div class="single-footer-widget section_padding_0_130">
+              <!-- Widget Title-->
+              <h5 class="widget-title">About</h5>
+              <!-- Footer Menu-->
+              <div class="footer_menu">
+                <ul>
+                  <li><a href="#">About Us</a></li>
+                  <li><a href="#">Corporate Sale</a></li>
+                  <li><a href="#">Terms &amp; Policy</a></li>
+                  <li><a href="#">Community</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <!-- Single Widget-->
+          <div class="col-12 col-sm-6 col-lg">
+            <div class="single-footer-widget section_padding_0_130">
+              <!-- Widget Title-->
+              <h5 class="widget-title">Support</h5>
+              <!-- Footer Menu-->
+              <div class="footer_menu">
+                <ul>
+                  <li><a href="#">Help</a></li>
+                  <li><a href="#">Support</a></li>
+                  <li><a href="#">Privacy Policy</a></li>
+                  <li><a href="#">Term &amp; Conditions</a></li>
+                  <li><a href="#">Help &amp; Support</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <!-- Single Widget-->
+          <div class="col-12 col-sm-6 col-lg">
+            <div class="single-footer-widget section_padding_0_130">
+              <!-- Widget Title-->
+              <h5 class="widget-title">Contact</h5>
+              <!-- Footer Menu-->
+              <div class="footer_menu">
+                <ul>
+                  <li><a href="#">Call Centre</a></li>
+                  <li><a href="#">Email Us</a></li>
+                  <li><a href="#">Term &amp; Conditions</a></li>
+                  <li><a href="#">Help Center</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <!-- jQuery(necessary for all JavaScript plugins)-->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/waypoints.min.js"></script>
+    <script src="js/jquery.easing.min.js"></script>
+    <script src="js/default/classy-nav.min.js"></script>
+    <script src="js/default/sticky.js"></script>
+    <script src="js/default/mail.js"></script>
+    <script src="js/default/scrollup.min.js"></script>
+    <script src="js/default/one-page-nav.js"></script>
+    <script src="js/jarallax.min.js"></script>
+    <script src="js/jarallax-video.min.js"></script>
+    <script src="js/jquery.counterup.min.js"></script>
+    <script src="js/jquery.countdown.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/wow.min.js"></script>
+    <script src="js/default/active.js"></script>
+  </body>
+
+<!-- Mirrored from demo.designing-world.com/apland-4.3.0/about-us.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 20 Feb 2020 08:49:12 GMT -->
+</html>
